9bc44ee0c8ff00e19c44a40d130c53cdf8c30be6
Ksenia Skarzhynets <109911970+witkcher@users.noreply.github.com>
2022-10-12 17:10:33 +0300

    add task 03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import trottle from 'lodash.throttle';
 
+const key = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import trottle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+
+const key = 'feedback-form-state';
+const formData = {};
+
+form.addEventListener('input', trottle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+populateInput();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  e.currentTarget.reset();
+  localStorage.removeItem(key);
+  console.log(formData);
+}
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(key, JSON.stringify(formData));
+}
+
+function populateInput(e) {
+  const savedMessage = localStorage.getItem(key);
+  const messageValue = JSON.parse(savedMessage);
+  if (savedMessage) {
+    form.elements.email.value = messageValue.email;
+    form.elements.message.value = messageValue.message;
+  }
+}
